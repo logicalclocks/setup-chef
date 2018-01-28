7c50396dbac8df7c3b5c473c9c225214403c8543
Jim Dowling <jdowling@sics.se>
2018-01-28 11:43:30 +0100

    fix

--- recipes/default.rb
+++ recipes/default.rb
@@ -193,7 +193,7 @@ bash "end_hops" do
     cat /tmp/cuda.hops >> /tmp/hops.hops
     echo -n " }" >> /tmp/hops.hops
     # JSON wants " (double quotes) not ' (single quotes)    
-    perl -pi -e "s/'/\"/g" /tmp/hops.hops
+    perl -pi -e \"s/'/\\"/g\" /tmp/hops.hops
     # cat /tmp/hops.hops | jq > /tmp/hops.pretty
     #mv -f /tmp/hops.pretty /tmp/hops.hops
   EOF
