d7f8ee3e35d0837ac0fb48b3f8455c0e48dc236d
Jim Dowling <jdowling@sics.se>
2018-02-10 00:54:09 +0100

    openssl

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -3,6 +3,7 @@ apt_update 'update' if platform_family?('debian')
 package 'curl'
 
 if "#{node['setup']['nginx_skip']}" != "true"
+  package 'openssl'
   node.override['nginx']['default_root'] = node['setup']['download_dir']
   node.override['nginx']['port'] = 1880
   include_recipe 'nginx::default'
