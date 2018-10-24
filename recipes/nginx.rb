86cb375ea7b68422aa71d3a6b591a6a1f8d9d514
Fabio Buso <buso.fabio@gmail.com>
2018-10-24 19:37:25 +0200

    test

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,38 +1,26 @@
 
-package 'curl'
+# package 'curl'
 
-case node['platform_family']
-when 'debian'
-  apt_update 'update'
-when 'rhel'
- Chef::Log.info "REDHAT !!!"
-  execute 'yum_update_upgrade' do
-   command 'sudo yum update -y'
-  end
-  yum_package 'openssl-libs'
-end
+# case node['platform_family']
+# when 'debian'
+#   apt_update 'update'
+# when 'rhel'
+#  Chef::Log.info "REDHAT !!!"
+#   execute 'yum_update_upgrade' do
+#    command 'sudo yum update -y'
+#   end
+#   yum_package 'openssl-libs'
+# end
+# 
+# 
+# if "#{node['setup']['nginx_skip']}" != "true"
+#   package 'openssl'
+#   node.override['nginx']['auth_request']['url'] = "#{node['download_url']}/nginx.tar.gz"
+#   node.override['nginx']['default_root'] = node['setup']['download_dir']
+#   node.override['nginx']['port'] = 1880
+#   include_recipe 'nginx::default'
+# end
 
-
-if "#{node['setup']['nginx_skip']}" != "true"
-  package 'openssl'
-  node.override['nginx']['auth_request']['url'] = "#{node['download_url']}/nginx.tar.gz"
-  node.override['nginx']['default_root'] = node['setup']['download_dir']
-  node.override['nginx']['port'] = 1880
-  include_recipe 'nginx::default'
-end
-
-def recursiveFlat(m)
-  values = m.values
-  ret_value = []
-  values.each do |v|
-    if v.instance_of? Hash
-      ret_value << recursiveFlat(v)
-    else
-      ret_value << v
-    end
-  end
-  ret_value
-end
 
 res = recursiveFlat(node)
 res.values.each do |v|
