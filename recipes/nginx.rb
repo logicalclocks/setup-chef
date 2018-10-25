a4da2a83d283f963f142b80b975d4e2b145ec815
Fabio Buso <buso.fabio@gmail.com>
2018-10-25 19:19:25 +0200

    Self discovery of artifacts to download

--- recipes/nginx.rb
+++ recipes/nginx.rb
@@ -1,34 +1,46 @@
+package 'wget'
 
-# package 'curl'
+case node['platform_family']
+when 'rhel'
+  package 'openssl-libs'
+end
 
-# case node['platform_family']
-# when 'debian'
-#   apt_update 'update'
-# when 'rhel'
-#  Chef::Log.info "REDHAT !!!"
-#   execute 'yum_update_upgrade' do
-#    command 'sudo yum update -y'
-#   end
-#   yum_package 'openssl-libs'
-# end
-# 
-# 
-# if "#{node['setup']['nginx_skip']}" != "true"
-#   package 'openssl'
-#   node.override['nginx']['auth_request']['url'] = "#{node['download_url']}/nginx.tar.gz"
-#   node.override['nginx']['default_root'] = node['setup']['download_dir']
-#   node.override['nginx']['port'] = 1880
-#   include_recipe 'nginx::default'
-# end
+if "#{node['setup']['nginx_skip']}" != "true"
+  package 'openssl'
+  node.override['nginx']['auth_request']['url'] = "#{node['download_url']}/nginx.tar.gz"
+  node.override['nginx']['default_root'] = node['setup']['download_dir']
+  node.override['nginx']['port'] = 1880
+  include_recipe 'nginx::default'
+end
 
+directory node['setup']['download_dir'] do
+  owner "root"
+  group "root"
+  recursive true
+  action :create
+end
+
+def recursiveFlat(m)
+  values = m.values
+  ret_value = []
+  values.each do |v|
+      if v.is_a? Hash
+        ret_value = ret_value | recursiveFlat(v)
+      else
+        ret_value << v
+      end
+  end
+  ret_value
+end
 
 res = recursiveFlat(node)
-res.values.each do |v|
-  if v =~ /#{node['download_url']}/
-    bash 'dump' do
+res.each do |v|
+  if v =~ /#{node['download_url']}.+/ || v =~ /http:\/\/snurran.sics.se\/hops\/.+/
+    bash "download-#{v}" do
       user 'root'
+      cwd node['setup']['download_dir']
       code <<-EOH
-        echo "#{v}" >> /tmp/urls
+        wget --mirror --no-parent -X "*" --reject "index.html*" -e robots=off --no-host-directories #{v}
       EOH
     end
   end
