1b1d2d7c41c1dfd0a82e8339b420579bba5da998
Jim Dowling <jdowling@sics.se>
2020-08-28 22:51:02 +0200

    fixes

--- recipes/gemserver.rb
+++ recipes/gemserver.rb
@@ -29,11 +29,11 @@ bash "start-gem-server" do
   user "root"
   code <<-EOH
       set -e
-      if [ ! -d #{node['setup']['gemserver']['version']} ] ; then
+      if [ ! -d /opt/chefdk/embedded/lib/ruby/gems/#{node['setup']['gemserver']['version']} ] ; then
          echo "Wrong gem server version. Set the correct attribute value for: setup/gemserver/version" 
          echo "Check in "ls /opt/chefdk/embedded/lib/ruby/gems/"
          exit 5
       fi
-      nohup /opt/chefdk/embedded/bin/gem server --dir --port 12345 /opt/chefdk/embedded/lib/ruby/gems/#{node['setup']['gemserver']['version']}/gems &
+      nohup /opt/chefdk/embedded/bin/gem server --dir --port #{node['setup']['gemserver']['port']} /opt/chefdk/embedded/lib/ruby/gems/#{node['setup']['gemserver']['version']}/gems &
       EOH
 end
