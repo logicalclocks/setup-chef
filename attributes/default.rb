4669b2c5b3b40e7ad0a614053867b99855679d0a
Fabio Buso <dev.siroibaf@gmail.com>
2022-12-23 10:17:18 +0100

    [cleanup_3.1] Cleanup (#23)
    Co-authored-by: Antonis Kouzoupis <kouzoupis.ant@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -19,25 +19,8 @@ include_attribute "consul"
 include_attribute "kube-hops"
 include_attribute "onlinefs"
 
-
-default['install']['dir']                          = ""
-default['install']['user']                         = ""
-default['install']['ssl']                          = "false"
-default['install']['cleanup_downloads']            = "false"
-default['install']['addhost']                      = "false"
-default['install']['hostname_prefix']              = "hops"
-
 default['kagent']['enabled']                       = "false"
 
-default['setup']['user']                           = node['install']['user'].empty? ? "vagrant" : node['install']['user']
-default['setup']['new_user']                       = "hdp"
-default['setup']['new_user_password']              = ""
-default['setup']['default']['public_ips']          = ['']
-default['setup']['default']['private_ips']         = ['']
-
-default['setup']['nginx']['skip']                  = "false"
-default['setup']['nginx']['download_dir']          = "/var/www/html"
-default['setup']['nginx']['port']                  = "1880"
-
-default['setup']['disable_nw_mgr']                 = "false"
-
+default['setup']['user']                           = "vagrant" 
+default['setup']['group']                          = "vagrant" 
+default['setup']['download_dir']                   = "/home/vagrant/dist" 
