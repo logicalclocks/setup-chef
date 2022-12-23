c356e6278f99e6f35fdefab21ac12ba0bf09fc72
Antonis Kouzoupis <antonios@logicalclocks.com>
2022-12-23 10:48:39 +0100

    Cleanup 3.1 (#24)
    * [remove_dela] Remove dela and add onlinefs (#22)
    
    * [cleanup_3.1] Cleanup (#23)
    
    Co-authored-by: Antonis Kouzoupis <kouzoupis.ant@gmail.com>
    
    Co-authored-by: Fabio Buso <dev.siroibaf@gmail.com>

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,7 +7,6 @@ include_attribute "elastic"
 include_attribute "glassfish"
 include_attribute "kkafka"
 include_attribute "kzookeeper"
-include_attribute "dela"
 include_attribute "hive2"
 include_attribute "hopsworks"
 include_attribute "kkafka"
@@ -18,26 +17,10 @@ include_attribute "hops"
 include_attribute "hops_airflow"
 include_attribute "consul"
 include_attribute "kube-hops"
-
-
-default['install']['dir']                          = ""
-default['install']['user']                         = ""
-default['install']['ssl']                          = "false"
-default['install']['cleanup_downloads']            = "false"
-default['install']['addhost']                      = "false"
-default['install']['hostname_prefix']              = "hops"
+include_attribute "onlinefs"
 
 default['kagent']['enabled']                       = "false"
 
-default['setup']['user']                           = node['install']['user'].empty? ? "vagrant" : node['install']['user']
-default['setup']['new_user']                       = "hdp"
-default['setup']['new_user_password']              = ""
-default['setup']['default']['public_ips']          = ['']
-default['setup']['default']['private_ips']         = ['']
-
-default['setup']['nginx']['skip']                  = "false"
-default['setup']['nginx']['download_dir']          = "/var/www/html"
-default['setup']['nginx']['port']                  = "1880"
-
-default['setup']['disable_nw_mgr']                 = "false"
-
+default['setup']['user']                           = "vagrant" 
+default['setup']['group']                          = "vagrant" 
+default['setup']['download_dir']                   = "/home/vagrant/dist" 
