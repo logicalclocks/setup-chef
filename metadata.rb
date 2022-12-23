636cbfec352016447c7e71d7809663e6db6e2fce
Fabio Buso <dev.siroibaf@gmail.com>
2022-12-23 10:17:18 +0100

    Cleanup (#23)
    Co-authored-by: Antonis Kouzoupis <kouzoupis.ant@gmail.com>

--- metadata.rb
+++ metadata.rb
@@ -12,7 +12,6 @@ source_url       "https://github.com/logicalclocks/setup-chef"
   supports os
 end
 
-depends 'nginx', '= 9.0.0'
 depends 'sudo', '~> 4.0.0'
 
 depends 'hopsworks'
@@ -37,68 +36,20 @@ depends 'hops_airflow'
 depends 'ulimit'
 depends 'consul'
 depends 'kube-hops'
+depends 'onlinefs'
 
-recipe "setup::install", "Fixes /etc/hosts and pings for connectivitiy"
-recipe "setup::default", "Checks memory, diskspace, returns a report to Karamel"
-recipe "setup::nginx", "Installs and configures nginx to host installation files"
-recipe "setup::new_user", "Installs a new user on all hosts"
-recipe "setup::master", "Installs a new user on this host and puts its public key on  other hosts"
-recipe "setup::purge", "Deletes cuda and everything else"
-
-
-attribute "install/dir",
-          :description => "Default ''. Set to a base directory under which all hops services will be installed.",
-          :type => "string"
-
-attribute "install/user",
-          :description => "User to install the services as",
-          :type => "string"
-
-attribute "install/ssl",
-          :description => "Is SSL turned on for all services?",
-          :type => "string"
-
-attribute "install/cleanup_downloads",
-          :description => "Remove any zipped binaries that were downloaded and used to install services",
-          :type => "string"
-
-attribute "install/addhost",
-          :description => "Indicates that this host will be added to an existing Hops cluster.",
-          :type => "string"
-
-attribute "install/hostname_prefix",
-          :description => "Add all ips to the /etc/hosts file and give each hostname this prefix. Default is 'hops'.",
-          :type => "string"
+recipe "setup::install", ""
+recipe "setup::default", "Download distribution artifacts"
 
 attribute "setup/user",
-          :description => "User to install the services as",
+  :description => "User creating and downloading the distribution binaries. (Default: vagrant)",
           :type => "string"
 
-attribute "setup/default/private_ips",
-          :description =>  "Ips for the hosts",
-          :type => 'array'
-
-attribute "setup/disable_nw_mgr",
-          :description =>  "Disable the NetworkManager service",
-          :type => 'string'
-
-attribute "setup/new_user",
-          :description => "New user to install  as",
+attribute "setup/group",
+  :description => "Group creating and downloading the distribution binaries. (Default: vagrant)",
           :type => "string"
 
-attribute "setup/new_user_password",
-          :description => "New user password",
-          :type => "string"
-
-attribute "setup/nginx/download_dir",
-          :description => "Dir to download binaries to",
-          :type => "string"
-
-attribute "setup/nginx/skip",
-          :description => "True to skip nginx installation",
-          :type => "string"
-
-attribute "setup/nginx/port",
-          :description => "Port on which nginx should listen",
+attribute "setup/download_dir",
+  :description => "Location where the distribution binaries will be downloaded. (Default: /home/vagrant/dist",
           :type => "string"
 
